381a514fd8caa311e46bb58ad37a26b22bd04f74
Lindos-Mahadi <lindosmahadi@gmail.com>
2020-09-19 21:41:04 +0600

    Updates

---
+++ precache-manifest.bcd80eefe4f4118b921702372794346b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624c75d0cc55de857ea1093741a44237",
+    "url": "/React-Second-ApplicationToDoList/index.html"
+  },
+  {
+    "revision": "66bd65bd2c13c8b56afb",
+    "url": "/React-Second-ApplicationToDoList/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "76690d4aed2973124994",
+    "url": "/React-Second-ApplicationToDoList/static/css/main.b68398a2.chunk.css"
+  },
+  {
+    "revision": "66bd65bd2c13c8b56afb",
+    "url": "/React-Second-ApplicationToDoList/static/js/2.be41b104.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/React-Second-ApplicationToDoList/static/js/2.be41b104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76690d4aed2973124994",
+    "url": "/React-Second-ApplicationToDoList/static/js/main.970bc64e.chunk.js"
+  },
+  {
+    "revision": "8b734e85675fec4be970",
+    "url": "/React-Second-ApplicationToDoList/static/js/runtime-main.cd660d82.js"
+  }
+]);
